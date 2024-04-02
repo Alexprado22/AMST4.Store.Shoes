f86aa1b2380b9ebe2707da680029d6308e390bf1
Gabriel <guardavege@gmail.com>
2024-04-02 18:16:34 -0300

    View Color Modelo 1

---
+++ Controllers/ColorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AMST4.Store.Shoes.Controllers;
+
+public class ColorController : Controller
+{
+    [HttpGet]
+    public ActionResult ColorIndex()
+    {
+        return View();
+    }
+
+   
+}
